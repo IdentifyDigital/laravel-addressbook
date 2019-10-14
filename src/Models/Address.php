a640818262680de896dce2062d6fa4385946fc72
yeti <yeti@identifywebdesign.co.uk>
2019-10-14 11:00:10 +0100

    Create the Address model __toString method.

--- src/Models/Address.php
+++ src/Models/Address.php
@@ -37,4 +37,51 @@ class Address extends Model
 		'lat'
 	];
 	
+	/**
+	 * Get this addressable as a string format. Squashes all
+	 * the different parts of the address into a single
+	 * string that can be rendered anywhere.
+	 *
+	 * @return String 	[ A text version of this addressable ]
+	 */
+	public function __toString ()
+	{
+		$parts = [];
+		
+		if ($this->building_name != '')
+			$parts[] = $this->building_name;
+		
+		if ($this->building_number != '')
+			$parts[] = $this->building_number;
+			
+		if ($this->line_1 != '')
+			$parts[] = $this->line_1;
+		
+		if ($this->line_2 != '')
+			$parts[] = $this->line_2;
+			
+		if ($this->line_3 != '')
+			$parts[] = $this->line_3;
+			
+		if ($this->line_4 != '')
+			$parts[] = $this->line_4;
+			
+		if ($this->town_or_city != '')
+			$parts[] = $this->town_or_city;
+			
+		if ($this->district != '')
+			$parts[] = $this->district;
+			
+		if ($this->county != '')
+			$parts[] = $this->county;
+		
+		if ($this->country != '')
+			$parts[] = $this->country;
+			
+		if ($this->post_code != '')
+			$parts[] = $this->post_code;
+		
+		return implode(', ', $parts);
+	}
+	
 }
