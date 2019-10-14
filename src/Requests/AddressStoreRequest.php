3effbb5e6be1e72769854e2a8688fb08ee3e5ab0
yeti <yeti@identifywebdesign.co.uk>
2019-10-14 11:48:14 +0100

    Update Request classes to handle address arrays.

--- src/Requests/AddressStoreRequest.php
+++ src/Requests/AddressStoreRequest.php
@@ -18,22 +18,22 @@ class AddressStoreRequest extends FormRequest
 	public function rules()
 	{
 		return [
-			'line_1' 		=> 'required|min:1',
-			'town_or_city' 	=> 'required|min:1',
-			'county' 		=> 'required|min:1',
-			'country'		=> 'required|min:1',
-			'post_code' 	=> 'required|min:1'
+			'address.line_1'		=> 'required|min:1',
+			'address.town_or_city'	=> 'required|min:1',
+			'address.county' 		=> 'required|min:1',
+			'address.country'		=> 'required|min:1',
+			'address.post_code' 	=> 'required|min:1'
 		];
 	}
 	
 	public function messages()
 	{
 		return [
-			'line_1.required' 		=> 'First line of Address is needed',
-			'town_or_city.required' => 'Town or City name is required',
-			'county'				=> 'County name is required',
-			'country'				=> 'Country is required',
-			'post_code'				=> 'Post code is required'
+			'address.line_1.required' 		=> 'First line of Address is needed',
+			'address.town_or_city.required' => 'Town or City name is required',
+			'address.county.required'		=> 'County name is required',
+			'address.country.required'		=> 'Country is required',
+			'address.post_code.required'	=> 'Post code is required'
 		];
 	}
 	
