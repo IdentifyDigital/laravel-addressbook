6138b0ead419e6a3ae14b2d42e049c33df766243
Josh Thackeray <josh@identifywebdesign.co.uk>
2020-04-30 12:35:05 +0100

    Added try/catch block to catch any error coming back from postcode lookup service

--- src/Observers/AddressObserver.php
+++ src/Observers/AddressObserver.php
@@ -3,6 +3,7 @@
 namespace IdentifyDigital\AddressBook\Observers;
 
 use IdentifyDigital\AddressBook\Models\Address;
+use Illuminate\Support\Facades\Log;
 use Jabranr\PostcodesIO\PostcodesIO;
 
 class AddressObserver
@@ -16,11 +17,15 @@ class AddressObserver
     public function creating(Address $address)
     {
         if(empty($address->lat) && empty($address->long)) {
-            $result = $this->lookupPostcodeLatLon($address->post_code);
+            try {
+                $result = $this->lookupPostcodeLatLon($address->post_code);
 
-            if(isset($result->result)) {
-                $address->long = $result->result->longitude;
-                $address->lat = $result->result->latitude;
+                if (isset($result->result)) {
+                    $address->long = $result->result->longitude;
+                    $address->lat = $result->result->latitude;
+                }
+            } catch (\Exception $e) {
+                Log::error("Error looking up postcode on address", ['error' => $e->getMessage()]);
             }
         }
     }
